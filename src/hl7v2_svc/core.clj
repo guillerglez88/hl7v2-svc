32d75be8bd25f19601d2a4139a91d658c0a158b9
Guillermo <guillerglez88@gmail.com>
2024-05-05 20:00:40 +0200

    hot reload routes

--- src/hl7v2_svc/core.clj
+++ src/hl7v2_svc/core.clj
@@ -18,24 +18,25 @@
 (defonce server (atom nil))
 
 (def app
-  (ring/ring-handler
-   (ring/router
-    routes
-    {:exception pretty/exception
-     :data {:coercion coercion
-            :muuntaja m/instance
-            :middleware [exception/exception-middleware
-                         parameters/parameters-middleware
-                         muuntaja/format-negotiate-middleware
-                         muuntaja/format-request-middleware
-                         muuntaja/format-response-middleware
-                         rrc/coerce-request-middleware
-                         rrc/coerce-response-middleware
-                         timbre/wrap-with-logger]}})
-   (ring/routes
-    (ring/redirect-trailing-slash-handler)
-    (ring/create-default-handler
-     {:not-found (constantly {:status 404, :body "Not found"})}))))
+  (ring/reloading-ring-handler
+   #(ring/ring-handler
+     (ring/router
+      [routes]
+      {:exception pretty/exception
+       :data {:coercion coercion
+              :muuntaja m/instance
+              :middleware [exception/exception-middleware
+                           parameters/parameters-middleware
+                           muuntaja/format-negotiate-middleware
+                           muuntaja/format-request-middleware
+                           muuntaja/format-response-middleware
+                           rrc/coerce-request-middleware
+                           rrc/coerce-response-middleware
+                           timbre/wrap-with-logger]}})
+     (ring/routes
+      (ring/redirect-trailing-slash-handler)
+      (ring/create-default-handler
+       {:not-found (constantly {:status 404, :body "Not found"})})))))
 
 (defn start! []
   (let [port (:port @cfg/config)]
--- src/hl7v2_svc/routes.clj
+++ src/hl7v2_svc/routes.clj
@@ -1,7 +1,14 @@
 (ns hl7v2-svc.routes)
 
+(defn root [_req]
+  {:status 200
+   :headers {"Content-Type" "text/html"}
+   :body "hi mom!"})
+
+(defn post-message [req]
+  {:status 200
+   :body (:body-params req)})
+
 (def routes
-  [["/" {:get {:handler (fn [_req]
-                          {:status 200
-                           :headers {"Content-Type" "text/html"}
-                           :body "hi mom!"})}}]])
+  [["/"         {:get {:handler root}}]
+   ["/Message"  {:post {:handler post-message}}]])
